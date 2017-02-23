488cdf745b16bed7a40ac802ebe55268b7fb42b7
CaRafa <c.rafa.14@gmail.com>
2017-02-23 12:21:19 -0400

    funcion de guardar en firebase

---
+++ public/js/funciones.js
@@ -0,0 +1,68 @@
+
+function crearusuario (email, password){
+
+ firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+ var user = firebase.auth().currentUser;
+
+ alert("usuario creado");
+ console.log(user.uid);
+ window.location.replace("/prueba.html");
+
+
+}),
+function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode == 'auth/weak-password') {
+    alert('The password is too weak.');
+  } 
+  else if (errorCode == 'auth/email-already-in-use'){
+  	alert('correo ya en uso');
+  }
+  else {
+    alert(errorMessage);
+  }
+
+}
+
+}
+
+
+
+function crearEntrada(){
+
+var nombre = document.getElementById('getnombre').value;
+var apellido = document.getElementById('getapellido').value;
+var cedula = document.getElementById('getcedula').value;
+var telefono = document.getElementById('gettelefono').value;
+var correo = document.getElementById('getemail').value;
+
+firebase.database().ref('entradas/'+cedula).set({
+  nombre:nombre,
+  apellido:apellido,
+  telefono:telefono,
+  correo:correo,
+}).then(function(){
+  window.location = "/";
+
+}, function(error){
+  alert(error.code);
+});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
